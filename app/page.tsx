0458b87953e6e123f6e3642623d111c2dd1abe41
haiba <omar.haiba@student.reutlingen-university.de>
2026-01-19 00:37:39 +0100

    Verbesserung

--- app/page.tsx
+++ app/page.tsx
@@ -224,19 +224,6 @@ export default function HomePage() {
               </li>
               <li className="money-indicator"><CoinStack /></li>
             </ul>
-            <div className="user-icon">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                <circle cx="12" cy="7" r="4"></circle>
-              </svg>
-            </div>
             <button
               className="hamburger"
               id="hamburger"
